137d27adbbab10ed7946af4aebed60adc5afe8ce
Akshay Patra <akshay.patra114@gmail.com>
2025-11-20 23:00:21 +0530

    MandiPricesDasboard- added a price comparison table for the analysis

--- src/Pages/Dashboard.js
+++ src/Pages/Dashboard.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import MandiMarketDashboardRedux from '../Mandi-components/MandiMarketDashboardRedux'
+import { Provider } from 'react-redux'
+import store from '../Mandi-components/store'
 
 export default function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div>Dashboard
+        <Provider store={store}>
+            <MandiMarketDashboardRedux />
+        </Provider>
+    </div>
   )
 }
